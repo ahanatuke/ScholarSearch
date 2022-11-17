9223c5e60c0a2110b625018973a87ed3976c8885
a-jutt <97474113+a-jutt@users.noreply.github.com>
2022-11-17 10:44:54 -0700

    Skeleton

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from math import floor
+
+
 def articles():
     print("Please enter in keywords for articles using spaces only")
     uI = input("> ").lower().split()
@@ -21,6 +24,7 @@ def venues():
     while check:
         try:
             uI = int(uI)
+            check = True
         except:
             print("The input is not a number, please try again"
                   "Enter the amount of top venues you would like to see")
@@ -34,9 +38,26 @@ def addArticle():
     venue = None
     references = []
     n_citations = 0
+    check = True
 
-    InId = input("Add an id\n> ")
-    tId = input("Add a title\n>")
+
+    id = input("Add an id\n> ").lower().strip()
+    title = input("Add a title\n>").lower().strip()
+    authors = input("Add the list of authors using spaces only\n>").lower().split()
+    year = input("Add the year for the article\n>").lower().strip()
+
+    while check:
+        if len(authors) < 1:
+            print("Please include at least one author.")
+            input("Add the list of authors using spaces only\n>").lower().split()
+        check = False
+    while check:
+        try:
+            yearInt = int(year)
+        except:
+            print("The year added is not a valid year, please try again.")
+            year = input("Add the year for the article\n>").lower().strip()
+        check = False
 
     return
 

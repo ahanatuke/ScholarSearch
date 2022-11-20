417a38b22f82ce93ba77f85349041f2f4a8e929e
“Beta” <zznnila11@gmail.com>
2022-11-20 01:56:59 -0800

    small changes to init_db()

--- initDB.py
+++ initDB.py
@@ -5,7 +5,7 @@ import json
 def init_db(jsonFile, portNum):
     portNum = 'mongodb://localhost:27017/'  # temp
     connection = MongoClient(portNum)
-    # connect to the server and will create a database named 291db (if it does not exist -- then what??)
+    # connect to the server and will create a database named 291db TODO (if it does not exist -- then what??)
     dbNames = connection.list_database_names()
     if '291db' not in dbNames:
         db = connection['291db']
@@ -19,7 +19,7 @@ def init_db(jsonFile, portNum):
         collection = db['dblp']
 
         # TODO Process it as one-row-at-a time, and not to fully load the file into memory
-        with open('file_name.json') as file:
+        with open(jsonFile) as file:
             data = json.load(file)
         # print(data)
         collection.insert_many(data)

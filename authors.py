29d3d61ca322f3d6b56f0bc80dc501930a7d8cd1
“Beta” <zznnila11@gmail.com>
2022-11-19 23:38:03 -0800

    moved functions to their respective multi-files

---
+++ authors.py
@@ -0,0 +1,6 @@
+def authors():
+    print("Please enter in keywords to search for authors using spaces only ")
+    uI = input("> ").lower().split()
+
+    # mongoDB here
+    return
--- main.py
+++ main.py
@@ -1,78 +1,7 @@
-from pymongo import MongoClient
-import json
-from math import floor
-
-
-def init_db():
-    client = MongoClient("mongodb://localhost:27017/")  # TODO do we need to connect to lab machine
-    db = client["291_proj2"]  # TODO do we need to take input for the name?
-
-    collection = db['data']
-    with open('file_name.json') as file:
-        data = json.load(file)
-    # print(clothes_data)
-    collection.insert_many(data)
-
-
-def articles():
-    print("Please enter in keywords for articles using spaces only")
-    uI = input("> ").lower().split()
-
-    # mongoDB here
-    return
-
-
-def authors():
-    print("Please enter in keywords to search for authors using spaces only ")
-    uI = input("> ").lower().split()
-
-    # mongoDB here
-    return
-
-
-def venues():
-    print("Enter the amount of top venues you would like to see")
-    uI = input("> ")
-    check = True
-    while check:
-        try:
-            uI = int(uI)
-            check = True
-        except:
-            print("The input is not a number, please try again"
-                  "Enter the amount of top venues you would like to see")
-            uI = input("> ")
-    # DB here
-    return
-
-
-def addArticle():
-    abstract = None
-    venue = None
-    references = []
-    n_citations = 0
-    check = True
-
-
-    id = input("Add an id\n> ").lower().strip()
-    title = input("Add a title\n>").lower().strip()
-    authors = input("Add the list of authors using spaces only\n>").lower().split()
-    year = input("Add the year for the article\n>").lower().strip()
-
-    while check:
-        if len(authors) < 1:
-            print("Please include at least one author.")
-            input("Add the list of authors using spaces only\n>").lower().split()
-        check = False
-    while check:
-        try:
-            yearInt = int(year)
-        except:
-            print("The year added is not a valid year, please try again.")
-            year = input("Add the year for the article\n>").lower().strip()
-        check = False
-
-    return
+import phase1
+import articles
+import authors
+import venues
 
 
 def main():
@@ -82,7 +11,7 @@ def main():
           "To add an article enter 'R'"
           "To quit enter 'Q'")
     done = False
-    while done == False:
+    while not done:
         uI = input("> ").lower().strip()
         if uI == 'a':
             pass

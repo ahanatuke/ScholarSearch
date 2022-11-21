675ea8920333c7900eb42d956743e35e95097f9a
“Beta” <zznnila11@gmail.com>
2022-11-20 18:44:49 -0800

    added TODOs to all functions

--- authors.py
+++ authors.py
@@ -1,6 +1,13 @@
-def authors():
+def searchAuthors():
     print("Please enter in keywords to search for authors using spaces only ")
     uI = input("> ").lower().split()
 
     # mongoDB here
+    # TODO Provide a keyword and see all authors whose names contain the keyword (the matches should be case-insensitive)
+
+    # TODO For each author, list the author name and the number of publications
+
+    # TODO The user should be able to select an author and see the title, year and venue of all articles by that author
+    '''The result should be sorted based on year with more recent articles shown first '''
+
     return

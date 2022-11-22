a08d306f53e77806920ae71e61c4a9105ec142e0
Anya Hanatuke <anyahanatuke@gmail.com>
2022-11-22 14:14:45 -0700

    added else to handle if db already exists, added comments

--- initDB.py
+++ initDB.py
@@ -3,15 +3,21 @@ import json
 
 
 def init_db(jsonFile, portNum):
-    portNum = 'mongodb://localhost:27017/'  # temp
+    portNum = 'mongodb://localhost:27017/'  # temp todo remove this before submit
     connection = MongoClient(portNum)
-    # connect to the server and will create a database named 291db TODO (if it does not exist -- then what??)
+    # connect to the server and will create a database named 291db
+    # implemented in such a way that if it *is* in list_db_names, it just connects to that one, checks for dblp, if it exists, it drops it
+    # i don't need to explain this all to you guys you're smart
     dbNames = connection.list_database_names()
+    # afai can tell, there's no real difference between handling for 291db existing vs handling for it *not* existing,
+    # might be worth removing this but also i am real loopy
     if '291db' not in dbNames:
         db = connection['291db']
 
         # If the collection exists, your program should drop it and create a new collection
         collectionsList = db.list_collection_names()  # Return a list of collections in '291db'
+        # this will never work afaiu, as if 291db doesn't exist as a db, it can't have the collection dblp
+        # todo add an elif statement that covers the other case
         if 'dblp' in collectionsList:
             col = db['dblp']
             col.drop()
@@ -41,5 +47,12 @@ def init_db(jsonFile, portNum):
         # collection.insert_many(data)
 
     else:
-        print('Database 291db already exists.\n Exiting program...')
-        exit()
+        db = connection['291db']
+        collectionsList = db.list_collection_names()
+
+        if 'dblp' in collectionsList:
+            col = db['dblp']
+            col.drop()
+
+        collection = db['dblp']
+        #todo populate db

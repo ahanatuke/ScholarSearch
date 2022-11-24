968b3b51b0056ca3de2dec500c15c398ed803df2
a-jutt <97474113+a-jutt@users.noreply.github.com>
2022-11-24 16:26:22 -0700

    mm

--- venues.py
+++ venues.py
@@ -4,8 +4,10 @@ import json
 import itertools
 import os
 
+
 def listVenues(collection, db):
-    print("Enter the amount of top venues you would like to see\nHit ENTER to go back to the mainpage\nHit E to exit the program")
+    print(
+        "Enter the amount of top venues you would like to see\nHit ENTER to go back to the mainpage\nHit E to exit the program")
     uI = input("> ")
     check = True
     while check:
@@ -25,15 +27,36 @@ def listVenues(collection, db):
     # TODO The user should be able to enter a number n and see a listing of top n venues
     a = db['venues']
     result = a.aggregate([
-     {"$sort": {"n_references": -1}},
-     {"$limit": intuI},
+        {"$sort": {"n_references": -1}},
+        {"$limit": intuI},
     ])
     result = list(result)
-    print(result)
-    for item in result:
-        print(item)
-    return
+    #
+    # i = 0
 
+    # for item in result:
+    #     r = collection.aggregate([
+    #         {"$venues": item["_id"]},
+    #     ])
+    #     r = list(r)
+    #     print(item, r)
+    #     i += 1
+    #     print(item)
 
+    all_top_venues = list(collection.aggregate([{
+        "$group": {
+            "_id": "$venue",
+            "count": {"$sum": 1},
+        }
+    }, {"$sort": {"count": -1}, }]))
 
-
+    print(f"row | venue | count | n_ref")
+    for i in range(1, int(uI)+1):
+        itm = all_top_venues[i]
+        for r in result:
+            if r['_id'] == itm['_id']:
+                n_ref = itm['n_references']
+                break
+        else: # no break
+            n_ref = 0
+        print(f"{i} | {itm['_id']} | {itm['count']} | {n_ref}")
